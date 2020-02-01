403823d09a77e3377dc2e862144339e4dc47c712
Luis-Moreno-Restrepo <60292100+Luis-Moreno-Restrepo@users.noreply.github.com>
2020-02-01 08:54:47 -0500

    Solucion ejercicios python: condicionales

---
+++ Ejercicios/Luis Moreno/Ejercicio16.py
@@ -0,0 +1,13 @@
+name = input("¿como te llamas? ")
+gender = input("¿cual es tu sexo (M o H)? ")
+if gender == "M":
+    if name.lower() < "m":
+        group = "A"
+    else:
+        group = "B"
+else:
+    if name.lower() > "n":
+        group = "A"
+    else:
+        group = "B"
+print("tu grupo es " + group)
