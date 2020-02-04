5bfab6cf326acba5786707377cf319d25b0202bf
camilo-paez <camilopaezb@gmail.com>
2020-02-04 11:40:14 -0500

    ejercicos bucles

---
+++ Ejercicios/CAMILO PAEZ/ejercicio18.py
@@ -0,0 +1,5 @@
+word = input("Introduce una palabra: ")
+for i in range(10):
+    print(word)
+
+    
---
+++ Ejercicios/CAMILO PAEZ/ejercicio23.py
@@ -0,0 +1,3 @@
+n = int(input("Introduce la altura del triangulo (entero positivo): "))
+for i in range(n):
+    print("*"*(i+1))
