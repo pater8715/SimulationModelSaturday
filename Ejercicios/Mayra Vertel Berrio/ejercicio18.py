d6c8351f980bbb1abf1d6e9de784289459a07019
male2197 <60292460+male2197@users.noreply.github.com>
2020-02-07 22:03:57 -0500

    Ejercicios de Python: Condicionales

---
+++ Ejercicios/Mayra Vertel Berrio/ejercicio18.py
@@ -0,0 +1,12 @@
+income = float(input("¿Cuál es tu renta anual? "))
+if income < 10000:
+    tax = 5
+elif income < 20000:
+    tax = 15
+elif income < 35000:
+    tax = 20
+elif income < 60000:
+    tax = 30
+else:
+    tax = 45
+print("Tu tipo impositivo es " + str(tax) + "%")
